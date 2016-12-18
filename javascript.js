afbcbc0f57bd66e46282e5e0784288bf03c5da86
KaypeoLoL <marinokolaric@outlook.com>
2016-12-18 05:19:28 +0100

    dodan audio

--- javascript.js
+++ javascript.js
@@ -3,7 +3,6 @@ function randomNumber(){
   var rand1 = Math.floor((Math.random()*6)+1);
   var rand2 = Math.floor((Math.random()*6)+1);
 
-
     var broj1 = document.getElementById('broj1').innerHTML=rand1;
     var broj2 = document.getElementById('broj2').innerHTML=rand2;
 
